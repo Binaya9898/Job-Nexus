2c420a160a6bd4997afa2c5e6e23e6117102db8f
Binaya9898 <82639575+Binaya9898@users.noreply.github.com>
2024-05-17 23:49:58 +0545

    Feat: More Nav Menu pages Added

--- App/More/More.js
+++ App/More/More.js
@@ -1,49 +1,158 @@
-import { View, Text, FlatList } from "react-native";
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import SERVER from "../../constants/server";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  FlatList,
+  Alert,
+} from "react-native";
+const More = ({ navigation }) => {
+  const data = [
+    {
+      id: 1,
+      name: "Comunity",
+      image: "https://img.icons8.com/clouds/100/000000/groups.png",
+      count: 124.711,
+      destination: "Login",
+    },
+    {
+      id: 2,
+      name: "Housing",
+      image: "https://img.icons8.com/color/100/000000/real-estate.png",
+      count: 234.722,
+    },
+    {
+      id: 3,
+      name: "Jobs",
+      image: "https://img.icons8.com/color/100/000000/find-matching-job.png",
+      count: 324.723,
+    },
+    {
+      id: 4,
+      name: "Personal",
+      image: "https://img.icons8.com/clouds/100/000000/employee-card.png",
+      count: 154.573,
+    },
+    {
+      id: 5,
+      name: "For sale",
+      image: "https://img.icons8.com/color/100/000000/land-sales.png",
+      count: 124.678,
+    },
+  ];
 
-const More = () => {
-  const [jobs, setJobs] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        console.log("Fetching");
-        const serverUrl = SERVER.primaryUrl + "/job-api";
-        console.log("Result is served by: " + serverUrl);
-        // const response = await axios.get(SERVER.primaryUrl + "/job-api");
-        const response = await axios.get(serverUrl);
-        // const response = await axios.get(serverUrl);
-        console.log(jobs);
-        setJobs(response.data);
-        console.log(jobs);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-    fetchData();
-    console.log("Jobs Fetched ", jobs);
-  }, []);
-  const renderItem = ({ item }) => (
-    <View>
-      {/* style={{ padding: 10, borderBottomWidth: 1, borderBottomColor: "#ccc" }} */}
-      <Text>{item.job_title}</Text>
-      <Text>{item.job_description}</Text>
-      <Text>{item.job_category}</Text>
-      <Text>{item.job_address}</Text>
-    </View>
-  );
+  const [options, setOptions] = useState(data);
+
+  const clickEventListener = (item) => {
+    navigation.navigate("Login", item.destination);
+  };
+
   return (
-    <View>
-      <Text>Jobs List</Text>
+    <View style={styles.container}>
       <FlatList
-        data={jobs}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id.toString()}
+        style={styles.contentList}
+        columnWrapperStyle={styles.listContainer}
+        data={options}
+        keyExtractor={(item) => {
+          return item.id;
+        }}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity
+              style={styles.card}
+              onPress={() => clickEventListener(item)}
+            >
+              <Image style={styles.image} source={{ uri: item.image }} />
+              <View style={styles.cardContent}>
+                <Text style={styles.name}>{item.name}</Text>
+                <Text style={styles.count}>{item.count}</Text>
+                <TouchableOpacity
+                  style={styles.followButton}
+                  onPress={() => clickEventListener(item)}
+                >
+                  <Text style={styles.followButtonText}>Explore now</Text>
+                </TouchableOpacity>
+              </View>
+            </TouchableOpacity>
+          );
+        }}
       />
     </View>
   );
-  return <Text>More Page</Text>;
 };
 
 export default More;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 20,
+    backgroundColor: "#ebf0f7",
+  },
+  contentList: {
+    flex: 1,
+  },
+  cardContent: {
+    marginLeft: 20,
+    marginTop: 10,
+  },
+  image: {
+    width: 90,
+    height: 90,
+    borderRadius: 45,
+    borderWidth: 2,
+    borderColor: "#ebf0f7",
+  },
+
+  card: {
+    shadowColor: "#00000021",
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.37,
+    shadowRadius: 7.49,
+    elevation: 12,
+
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 20,
+    backgroundColor: "white",
+    padding: 10,
+    flexDirection: "row",
+    borderRadius: 30,
+  },
+
+  name: {
+    fontSize: 18,
+    flex: 1,
+    alignSelf: "center",
+    color: "#3399ff",
+    fontWeight: "bold",
+  },
+  count: {
+    fontSize: 14,
+    flex: 1,
+    alignSelf: "center",
+    color: "#6666ff",
+  },
+  followButton: {
+    marginTop: 10,
+    height: 35,
+    width: 100,
+    padding: 10,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 30,
+    backgroundColor: "white",
+    borderWidth: 1,
+    borderColor: "#dcdcdc",
+  },
+  followButtonText: {
+    color: "#dcdcdc",
+    fontSize: 12,
+  },
+});
