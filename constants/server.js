2a87aa893cbdbfc90bc4c88393207a44da4fd9f7
Binaya9898 <82639575+Binaya9898@users.noreply.github.com>
2024-05-27 11:04:56 +0545

    Debug:Sending data to backend failed

--- constants/server.js
+++ constants/server.js
@@ -1,4 +1,5 @@
 const SERVER = {
-  primaryUrl: "http://192.168.1.66:8000/api",
+  secondaryUrl: "http://192.168.1.17:8000/api",
+  primaryUrl: "http://127.0.0.1:8000/api",
 };
 export default SERVER;
